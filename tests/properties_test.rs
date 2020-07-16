4c3ba3df7a0120ef025658213988f82d782af5b7
Yujeonja <hardboiled65@gmail.com>
2020-07-16 17:13:32 +0900

    Reorder property test by alphabetically.

--- tests/properties_test.rs
+++ tests/properties_test.rs
@@ -87,111 +87,143 @@ fn check_properties(cp: CodePoint, el: &Vec<OwnedAttribute>) -> bool {
     //================================================
     // Binary Properties
     //================================================
-    // WSpace (White_Space)
-    let cp_wspace = BinaryProperty::from(cp.wspace()).property_value_name().abbr;
-    check_property(cp, cp_wspace, "WSpace", el);
-    // Bidi_C (Bidi_Control)
-    let cp_bidi_c = BinaryProperty::from(cp.bidi_c()).property_value_name().abbr;
-    check_property(cp, cp_bidi_c, "Bidi_C", el);
-    // Join_C (Join_Control)
-    let cp_join_c = BinaryProperty::from(cp.join_c()).property_value_name().abbr;
-    check_property(cp, cp_join_c, "Join_C", el);
-    // Dash (Dash)
-    let cp_dash = BinaryProperty::from(cp.dash()).property_value_name().abbr;
-    check_property(cp, cp_dash, "Dash", el);
-    // Hyphen (Hyphen)
-    let cp_hyphen = BinaryProperty::from(cp.hyphen()).property_value_name().abbr;
-    check_property(cp, cp_hyphen, "Hyphen", el);
-    // QMark (Quotation_Mark)
-    let cp_qmark = BinaryProperty::from(cp.qmark()).property_value_name().abbr;
-    check_property(cp, cp_qmark, "QMark", el);
-    // Term (Terminal_Punctuation)
-    let cp_term = BinaryProperty::from(cp.term()).property_value_name().abbr;
-    check_property(cp, cp_term, "Term", el);
-    // OMath (Other_Math)
-    let cp_omath = BinaryProperty::from(cp.omath()).property_value_name().abbr;
-    check_property(cp, cp_omath, "OMath", el);
-    // Hex (Hex_Digit)
-    let cp_hex = BinaryProperty::from(cp.hex()).property_value_name().abbr;
-    check_property(cp, cp_hex, "Hex", el);
     // AHex (ASCII_Hex_Digit)
     let cp_ahex = BinaryProperty::from(cp.ahex()).property_value_name().abbr;
     check_property(cp, cp_ahex, "AHex", el);
-    // OAlpha (Other_Alphabetic)
-    let cp_oalpha = BinaryProperty::from(cp.oalpha()).property_value_name().abbr;
-    check_property(cp, cp_oalpha, "OAlpha", el);
-    // Ideo (Ideographic)
-    let cp_ideo = BinaryProperty::from(cp.ideo()).property_value_name().abbr;
-    check_property(cp, cp_ideo, "Ideo", el);
+    // Alpha                    ; Alphabetic
+    // Bidi_C (Bidi_Control)
+    let cp_bidi_c = BinaryProperty::from(cp.bidi_c()).property_value_name().abbr;
+    check_property(cp, cp_bidi_c, "Bidi_C", el);
+    // Bidi_M                   ; Bidi_Mirrored
+    // Cased                    ; Cased
+    // CE                       ; Composition_Exclusion
+    // CI                       ; Case_Ignorable
+    // Comp_Ex                  ; Full_Composition_Exclusion
+    // CWCF                     ; Changes_When_Casefolded
+    // CWCM                     ; Changes_When_Casemapped
+    // CWKCF                    ; Changes_When_NFKC_Casefolded
+    // CWL                      ; Changes_When_Lowercased
+    // CWT                      ; Changes_When_Titlecased
+    // CWU                      ; Changes_When_Uppercased
+    // Dash (Dash)
+    let cp_dash = BinaryProperty::from(cp.dash()).property_value_name().abbr;
+    check_property(cp, cp_dash, "Dash", el);
+    // Dep (Deprecated)
+    let cp_dep = BinaryProperty::from(cp.dep()).property_value_name().abbr;
+    check_property(cp, cp_dep, "Dep", el);
+    // DI                       ; Default_Ignorable_Code_Point
     // Dia (Diacritic)
     let cp_dia = BinaryProperty::from(cp.dia()).property_value_name().abbr;
     check_property(cp, cp_dia, "Dia", el);
+    // EBase                    ; Emoji_Modifier_Base
+    // EComp                    ; Emoji_Component
+    // EMod                     ; Emoji_Modifier
+    // Emoji                    ; Emoji
+    // EPres                    ; Emoji_Presentation
     // Ext (Extender)
     let cp_ext = BinaryProperty::from(cp.ext()).property_value_name().abbr;
     check_property(cp, cp_ext, "Ext", el);
-    // OLower (Other_Lowercase)
-    let cp_olower = BinaryProperty::from(cp.olower()).property_value_name().abbr;
-    check_property(cp, cp_olower, "OLower", el);
-    // OUpper (Other_Uppercase)
-    let cp_oupper = BinaryProperty::from(cp.oupper()).property_value_name().abbr;
-    check_property(cp, cp_oupper, "OUpper", el);
-    // NChar (Noncharacter_Code_Point)
-    let cp_nchar = BinaryProperty::from(cp.nchar()).property_value_name().abbr;
-    check_property(cp, cp_nchar, "NChar", el);
-    // OGr_Ext (Other_Grapheme_Extend)
-    let cp_ogr_ext = BinaryProperty::from(cp.ogr_ext()).property_value_name().abbr;
-    check_property(cp, cp_ogr_ext, "OGr_Ext", el);
+    // ExtPict                  ; Extended_Pictographic
+    // Gr_Base                  ; Grapheme_Base
+    // Gr_Ext                   ; Grapheme_Extend
+    // Gr_Link                  ; Grapheme_Link
+    // Hex (Hex_Digit)
+    let cp_hex = BinaryProperty::from(cp.hex()).property_value_name().abbr;
+    check_property(cp, cp_hex, "Hex", el);
+    // Hyphen (Hyphen)
+    let cp_hyphen = BinaryProperty::from(cp.hyphen()).property_value_name().abbr;
+    check_property(cp, cp_hyphen, "Hyphen", el);
+    // IDC                      ; ID_Continue
+    // Ideo (Ideographic)
+    let cp_ideo = BinaryProperty::from(cp.ideo()).property_value_name().abbr;
+    check_property(cp, cp_ideo, "Ideo", el);
+    // IDS                      ; ID_Start
     // IDSB (IDS_Binary_Operator)
     let cp_idsb = BinaryProperty::from(cp.idsb()).property_value_name().abbr;
     check_property(cp, cp_idsb, "IDSB", el);
     // IDST (IDS_Trinary_Operator)
     let cp_idst = BinaryProperty::from(cp.idst()).property_value_name().abbr;
     check_property(cp, cp_idst, "IDST", el);
-    // Radical (Radical)
-    let cp_radical = BinaryProperty::from(cp.radical()).property_value_name().abbr;
-    check_property(cp, cp_radical, "Radical", el);
-    // UIdeo (Unified_Ideograph)
-    let cp_uideo = BinaryProperty::from(cp.uideo()).property_value_name().abbr;
-    check_property(cp, cp_uideo, "UIdeo", el);
-    // ODI (Other_Default_Ignorable_Code_Point)
-    let cp_odi = BinaryProperty::from(cp.odi()).property_value_name().abbr;
-    check_property(cp, cp_odi, "ODI", el);
-    // Dep (Deprecated)
-    let cp_dep = BinaryProperty::from(cp.dep()).property_value_name().abbr;
-    check_property(cp, cp_dep, "Dep", el);
-    // SD (Soft_Dotted)
-    let cp_sd = BinaryProperty::from(cp.sd()).property_value_name().abbr;
-    check_property(cp, cp_sd, "SD", el);
+    // Join_C (Join_Control)
+    let cp_join_c = BinaryProperty::from(cp.join_c()).property_value_name().abbr;
+    check_property(cp, cp_join_c, "Join_C", el);
     // LOE (Logical_Order_Exception)
     let cp_loe = BinaryProperty::from(cp.loe()).property_value_name().abbr;
     check_property(cp, cp_loe, "LOE", el);
-    // OIDS (Other_ID_Start)
-    let cp_oids = BinaryProperty::from(cp.oids()).property_value_name().abbr;
-    check_property(cp, cp_oids, "OIDS", el);
-    // OIDC (Other_ID_Continue)
-    let cp_oidc = BinaryProperty::from(cp.oidc()).property_value_name().abbr;
-    check_property(cp, cp_oidc, "OIDC", el);
-    // STerm (Sentence_Terminal)
-    let cp_sterm = BinaryProperty::from(cp.sterm()).property_value_name().abbr;
-    check_property(cp, cp_sterm, "STerm", el);
-    // VS (Variation_Selector)
-    let cp_vs = BinaryProperty::from(cp.vs()).property_value_name().abbr;
-    check_property(cp, cp_vs, "VS", el);
-    // Pat_WS (Pattern_White_Space)
-    let cp_pat_ws = BinaryProperty::from(cp.pat_ws()).property_value_name().abbr;
-    check_property(cp, cp_pat_ws, "Pat_WS", el);
-    // Pat_Syn (Pattern_Syntax)
-    let cp_pat_syn = BinaryProperty::from(cp.pat_syn()).property_value_name().abbr;
-    check_property(cp, cp_pat_syn, "Pat_Syn", el);
-    // PCM (Prepended_Concatenation_Mark)
-    let cp_pcm = BinaryProperty::from(cp.pcm()).property_value_name().abbr;
-    check_property(cp, cp_pcm, "PCM", el);
-    // RI (Regional_Indicator)
-    let cp_ri = BinaryProperty::from(cp.ri()).property_value_name().abbr;
-    check_property(cp, cp_ri, "RI", el);
+    // Lower                    ; Lowercase
     // Math (Math)
     let cp_math = BinaryProperty::from(cp.math()).property_value_name().abbr;
     check_property(cp, cp_math, "Math", el);
+    // NChar (Noncharacter_Code_Point)
+    let cp_nchar = BinaryProperty::from(cp.nchar()).property_value_name().abbr;
+    check_property(cp, cp_nchar, "NChar", el);
+    // OAlpha (Other_Alphabetic)
+    let cp_oalpha = BinaryProperty::from(cp.oalpha()).property_value_name().abbr;
+    check_property(cp, cp_oalpha, "OAlpha", el);
+    // ODI (Other_Default_Ignorable_Code_Point)
+    let cp_odi = BinaryProperty::from(cp.odi()).property_value_name().abbr;
+    check_property(cp, cp_odi, "ODI", el);
+    // OGr_Ext (Other_Grapheme_Extend)
+    let cp_ogr_ext = BinaryProperty::from(cp.ogr_ext()).property_value_name().abbr;
+    check_property(cp, cp_ogr_ext, "OGr_Ext", el);
+    // OIDC (Other_ID_Continue)
+    let cp_oidc = BinaryProperty::from(cp.oidc()).property_value_name().abbr;
+    check_property(cp, cp_oidc, "OIDC", el);
+    // OIDS (Other_ID_Start)
+    let cp_oids = BinaryProperty::from(cp.oids()).property_value_name().abbr;
+    check_property(cp, cp_oids, "OIDS", el);
+    // OLower (Other_Lowercase)
+    let cp_olower = BinaryProperty::from(cp.olower()).property_value_name().abbr;
+    check_property(cp, cp_olower, "OLower", el);
+    // OMath (Other_Math)
+    let cp_omath = BinaryProperty::from(cp.omath()).property_value_name().abbr;
+    check_property(cp, cp_omath, "OMath", el);
+    // OUpper (Other_Uppercase)
+    let cp_oupper = BinaryProperty::from(cp.oupper()).property_value_name().abbr;
+    check_property(cp, cp_oupper, "OUpper", el);
+    // Pat_Syn (Pattern_Syntax)
+    let cp_pat_syn = BinaryProperty::from(cp.pat_syn()).property_value_name().abbr;
+    check_property(cp, cp_pat_syn, "Pat_Syn", el);
+    // Pat_WS (Pattern_White_Space)
+    let cp_pat_ws = BinaryProperty::from(cp.pat_ws()).property_value_name().abbr;
+    check_property(cp, cp_pat_ws, "Pat_WS", el);
+    // PCM (Prepended_Concatenation_Mark)
+    let cp_pcm = BinaryProperty::from(cp.pcm()).property_value_name().abbr;
+    check_property(cp, cp_pcm, "PCM", el);
+     // QMark (Quotation_Mark)
+    let cp_qmark = BinaryProperty::from(cp.qmark()).property_value_name().abbr;
+    check_property(cp, cp_qmark, "QMark", el);
+    // Radical (Radical)
+    let cp_radical = BinaryProperty::from(cp.radical()).property_value_name().abbr;
+    check_property(cp, cp_radical, "Radical", el);
+    // RI (Regional_Indicator)
+    let cp_ri = BinaryProperty::from(cp.ri()).property_value_name().abbr;
+    check_property(cp, cp_ri, "RI", el);
+    // SD (Soft_Dotted)
+    let cp_sd = BinaryProperty::from(cp.sd()).property_value_name().abbr;
+    check_property(cp, cp_sd, "SD", el);
+    // STerm (Sentence_Terminal)
+    let cp_sterm = BinaryProperty::from(cp.sterm()).property_value_name().abbr;
+    check_property(cp, cp_sterm, "STerm", el);
+    // Term (Terminal_Punctuation)
+    let cp_term = BinaryProperty::from(cp.term()).property_value_name().abbr;
+    check_property(cp, cp_term, "Term", el);
+    // UIdeo (Unified_Ideograph)
+    let cp_uideo = BinaryProperty::from(cp.uideo()).property_value_name().abbr;
+    check_property(cp, cp_uideo, "UIdeo", el);
+    // Upper                    ; Uppercase
+    // VS (Variation_Selector)
+    let cp_vs = BinaryProperty::from(cp.vs()).property_value_name().abbr;
+    check_property(cp, cp_vs, "VS", el);
+    // WSpace (White_Space)
+    let cp_wspace = BinaryProperty::from(cp.wspace()).property_value_name().abbr;
+    check_property(cp, cp_wspace, "WSpace", el);
+    // XIDC                     ; XID_Continue
+    // XIDS                     ; XID_Start
+    // XO_NFC                   ; Expands_On_NFC
+    // XO_NFD                   ; Expands_On_NFD
+    // XO_NFKC                  ; Expands_On_NFKC
+    // XO_NFKD                  ; Expands_On_NFKD
 
     true
 }
