4247024f814206e2e1c379400f0d4ceaee9a2f36
Yujeonja <hardboiled65@gmail.com>
2020-07-21 22:07:41 +0900

    Property dm test passed.

--- src/unicode/hangul.rs
+++ src/unicode/hangul.rs
@@ -1,3 +1,7 @@
+use crate::unicode::CodePoint;
+use crate::unicode::Ucd;
+use crate::unicode::props::Hst;
+
 // Constants from Unicode 3.12 (Version 13.0.0)
 const S_BASE: u32 = 0xAC00;
 const L_BASE: u32 = 0x1100;
@@ -9,6 +13,32 @@ const V_COUNT: u32 = 21;
 const T_COUNT: u32 = 28;
 const N_COUNT: u32 = 588; // (VCount * TCount)
 const S_COUNT: u32 = 11172; // (LCount * NCount)
+
+pub(crate) fn arithmetic_decomposition_mapping(s: u32) -> Vec<u32> {
+    let s_index = s - S_BASE;
+    match CodePoint::new(s).unwrap().hst() {
+        Hst::LV => {
+            let s_index = s - S_BASE;
+            let l_index = s_index / N_COUNT;
+            let v_index = (s_index % N_COUNT) / T_COUNT;
+
+            let l_part = L_BASE + l_index;
+            let v_part = V_BASE + v_index;
+
+            vec![l_part, v_part]
+        }
+        Hst::LVT => {
+            let lv_index = (s_index / T_COUNT) * T_COUNT;
+            let t_index = s_index % T_COUNT;
+
+            let lv_part = S_BASE + lv_index;
+            let t_part = T_BASE + t_index;
+
+            vec![lv_part, t_part]
+        }
+        _ => vec![],
+    }
+}
 
 pub(crate) fn decompose(s: u32) -> Vec<u32> {
     let s_index = s - S_BASE;
--- src/unicode/ucd/dm.rs
+++ src/unicode/ucd/dm.rs
@@ -1,7 +1,19 @@
 pub(self) mod dm_map;
 
+use crate::unicode::hangul;
+
 pub(crate) fn dm(cp: u32) -> String {
-    // For hangul syllables.
+    // Hangul syllables.
+    // AC00..D7AF; Hangul Syllables
+    // For performance, the range of block is hard coded.
+    if (0xAC00..=0xD7AF).contains(&cp) {
+        let decomposed = hangul::arithmetic_decomposition_mapping(cp);
+        let mut mapping = String::new();
+        for code in decomposed.iter() {
+            mapping.push(std::char::from_u32(*code).unwrap());
+        }
+        return mapping;
+    }
 
     let found = dm_map::DM_MAP.binary_search_by_key(&cp, |&(k,_)| k);
     match found {
